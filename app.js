b337f704abef1443e7f4c54d74df99fbf345a5ca
Gökan ŞİŞMAN <gknsismandenmark@gmail.com>
2021-03-10 14:51:49 +0200

    Update4

---
+++ app.js
@@ -0,0 +1,17 @@
+//getElementsByClassName ile sağ tarafta yer alan listelerimiz üzerinde oynamalar yaptık.
+
+
+
+//GETELEMENTSBYCLASSNAME İLE ELEMENTİMİZİ SEÇTİK.
+document.getElementsByClassName
+let items = document.getElementsByClassName('list-group-item');
+
+        //LİSTE GRUBU İÇİNDEKİ 2'NCİ SIRADAKİ ELEMENTİN İÇERİĞİNİ KENDİ İSTEDİĞİMİZ YAZI İLE DEĞİŞTİRDİK.
+        items[1].textContent =  `app.js dosyası ile bu elementin içeriğini değiştirdik.;`
+
+        //LİSTE GRUBU İÇİNDEKİ 4'NCÜ SIRADAKİ ELEMENTİN ARKA RENGİNİ MAVİ YAPTIK.
+        items[3].style.backgroundColor = 'blue';
+        
+
+        //LİSTE GRUBU İÇİNDEKİ 5'NCİ SIRADAKİ ELEMENTİN YAZI RENGİNİ KIRMIZI YAPTIK.
+        items[4].style.color='red'
---
+++ odev.md
@@ -0,0 +1,15 @@
+-   dom manupulating
+    -   index.html
+        -   body içerisinde hiçbir html elementi olmayacak. app.js ilave edilecek.
+    -   app.js dosyası olacak içerisinde
+        -   document.querySelector...
+        -   documunt.getElementById..
+        -   documunt.createElement..
+
+    
+        -div içerinde > form > onun içinde de inputs olsun. Yanında bir div daha olsun içinde de ul elementi ve içinde li olsun.
+
+        //OPTIONAL:
+        -   event eklensin. 
+
+
