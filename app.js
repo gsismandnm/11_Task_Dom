b00973536370011801a5953d88c6eda96a007ed0
Gökan ŞİŞMAN <gknsismandenmark@gmail.com>
2021-03-11 20:17:56 +0200

    Update 03.12.2021

--- app.js
+++ app.js
@@ -1,17 +1,22 @@
-//Yeni bir container oluşturduk.
+ //Yeni bir container oluşturduk.
 let container = document.createElement('div');
         container.className='container';
         //oluşturduğumuz container'ı body'e bağladık.
         document.body.appendChild(container);
+        //Sayfamızın arka sayfasını renklendirdik.
+        document.body.style.backgroundColor = "#31cabb";
 
-//container içine bir row yaptık.
+//container içine bir row oluşturduk.
 let row=document.createElement('div');
         row.className='row';
-        container.appendChild(row);
+        document.querySelector(".container").appendChild(row)
+        //container.appendChild(row);
 
-//yaptığımız row u 5-2-5'li columa ayırdık. İlk sol columu oluşturduk.
+//yaptığımız row u 5-2-5'li columa ayırdık. İlk sol columu oluşturduk. Rengini kırmızı yaptık.
 let colSol=document.createElement('div');
         colSol.className='col-md-5';
+        colSol.style.backgroundColor='red';
+        colSol.style.border='solid';
         row.appendChild(colSol);
 
 //divFrmGrp adında bir form oluşturduk ve onu columa ekledik.
@@ -20,38 +25,76 @@ let divFrmGrp =document.createElement('div');
         divFrmGrp.className='form-group';
         colSol.appendChild(divFrmGrp);
 
-//Email girmek için bir div oluşturup içine label ve input alanı oluşturduk.
+//Email için bir div oluşturup içine label alanı oluşturduk.
 let labelEmail =document.createElement('label');
         labelEmail.setAttribute("id", "labelEmail");
         labelEmail.htmlFor="inputEmail";
-        labelEmail.innerHTML="Email";
+        labelEmail.innerHTML="E-mail for Register";
         divFrmGrp.appendChild(labelEmail);
 
+//Email girebilmek için bir holder alan oluşturduk.
 let inputEmail=document.createElement('input');
-        inputEmail.type="text";
+        inputEmail.type="email";
+        inputEmail.id='inputEmail'
         inputEmail.className="form-control";
+        inputEmail.placeholder="Lütfen e-mail adresinizi giriniz."
         divFrmGrp.appendChild(inputEmail);
 
-//yaptığımız row u 5-2-5'li columa ayırdık. İlk orta columu oluşturduk.
+//Password girmek için label alanı oluşturduk.
+let labelSifre =document.createElement('label');
+        labelSifre.setAttribute("id", "labelSifre");
+        labelSifre.htmlFor="txtPassword";
+        labelSifre.innerHTML="Password";
+        divFrmGrp.appendChild(labelSifre);
+
+//Password yazabilmek için input kısmını oluşturduk.
+let inputSifre=document.createElement('input');
+        inputSifre.id='inputSifre';
+        inputSifre.type="password";
+        inputSifre.className="form-control";
+        inputSifre.placeholder="Lütfen şifrenizi giriniz @by Fat"
+        divFrmGrp.appendChild(inputSifre);
+
+//yaptığımız row u 5-2-5'li columa ayırmıştık. Orta columu oluşturduk.
 let colOrta=document.createElement('div');
         colOrta.className='col-md-2';
+        colOrta.style.borderBottom='solid';
+        colOrta.style.borderTop='solid';
         row.appendChild(colOrta);
+
+//orta colum içine "InfoTechAcademy" yazdık.
+let paraReklam=document.createElement('div')
+        paraReklam.textContent='InfoTechAcademy';
+        colOrta.appendChild(paraReklam);
 
 //yaptığımız row u 5-2-5'li columa ayırdık. İlk sağ columu oluşturduk.
 let colSag=document.createElement('div');
         colSag.className='col-md-5';
+        colSag.style.backgroundColor='yellow';
+        colSag.style.border='solid';
         row.appendChild(colSag);
 
+const newLocal = 'ul';
 //sağ tarafta oluşturduğumuz columun içine ul ekledik.
 let ulListe = document.createElement('ul');
         ulListe.className='list-group';
         colSag.appendChild(ulListe);
 
-//ul içine listeleri oluşturduk.
-let li=document.createElement('li');
-        li.className='list-group-item';
-        li.innerHTML="Birinci Sıra Liste";
-        li.appendChild(ulListe);
+//ul içine birinci listeyi oluşturduk.
+let liListe1=document.createElement('li');
+        liListe1.className='list-group-item';
+        liListe1.innerHTML="A Grubu İsim Listesi";
+        ulListe.appendChild(liListe1);
 
+//ul içine ikinci listeyi oluşturduk.
+let liListe2=document.createElement('li');
+        liListe2.className='list-group-item';
+        liListe2.innerHTML="B Grubu İsim Listesi";
+        liListe2.innerText='Deneme';
+        ulListe.appendChild(liListe2);
 
-
+//ul içine üçüncü listeyi oluşturduk.
+let liListe3=document.createElement('li');
+        liListe3.className='list-group-item';
+        liListe3.innerHTML="C Grubu İsim Listesi";
+        ulListe.appendChild(liListe3);
